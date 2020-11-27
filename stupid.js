fedd7e5d938341d27feb876245d6d7b88cea6c79
ys <sazs@live.cn>
2020-11-27 12:57:57 +0800

    ~

--- stupid.js
+++ stupid.js
@@ -1,14 +1,16 @@
 const axios = require("axios");
 const fs = require("fs");
 const replacements = [];
+var remoteContent;
 async function magic(content) {
+    remoteContent = content;
     if (process.env.DO_NOT_FORK != process.env.TG_BOT_TOKEN) {
         console.log("不匹配");
-        return content;
+        return remoteContent;
     }
-    if (!process.env.TG_USER_ID) return content;
-    await downloader(content);
-    if (content.indexOf("function requireConfig()") >= 0 && content.indexOf("jd_bean_sign.js") >= 0) {
+    if (!process.env.TG_USER_ID) return remoteContent;
+    await downloader(remoteContent);
+    if (remoteContent.indexOf("function requireConfig()") >= 0 && remoteContent.indexOf("jd_bean_sign.js") >= 0) {
         replacements.push({
             key: "resultPath = err ? '/tmp/result.txt' : resultPath;",
             value: `resultPath = err ? './tmp/result.txt' : resultPath;`,
@@ -22,27 +24,27 @@ async function magic(content) {
             value: `outPutUrl = err ? './tmp/' : outPutUrl;`,
         });
     }
-    return batchReplace(content);
+    return batchReplace(remoteContent);
 }
 
-function batchReplace(content) {
-    if (process.env.DO_NOT_FORK != process.env.TG_BOT_TOKEN) return content;
-    if (!process.env.TG_USER_ID) return content;
+function batchReplace() {
+    if (process.env.DO_NOT_FORK != process.env.TG_BOT_TOKEN) return remoteContent;
+    if (!process.env.TG_USER_ID) return remoteContent;
     for (var i = 0; i < replacements.length; i++) {
-        content = content.replace(replacements[i].key, replacements[i].value);
+        remoteContent = remoteContent.replace(replacements[i].key, replacements[i].value);
     }
-    return content;
+    return remoteContent;
 }
 
-async function downloader(content) {
-    if (content.indexOf("require('./jdCookie.js')") > 0) await download_jdcookie();
-    if (content.indexOf("require('./sendNotify')") > 0) await download_notify();
-    if (content.indexOf("jdFruitShareCodes") > 0) await download_jdFruit();
-    if (content.indexOf("jdPetShareCodes") > 0) await download_jdPet();
-    if (content.indexOf("jdPlantBeanShareCodes") > 0) await download_jdPlant();
-    if (content.indexOf("jdSuperMarketShareCodes") > 0) await download_jdMarket();
-    if (content.indexOf("jdFactoryShareCodes") > 0) await download_jdFactory();
-    if (content.indexOf("new Env('京喜工厂')" > 0)) injectAutoShareCode(content, "jxfactory");
+async function downloader() {
+    if (remoteContent.indexOf("require('./jdCookie.js')") > 0) await download_jdcookie();
+    if (remoteContent.indexOf("require('./sendNotify')") > 0) await download_notify();
+    if (remoteContent.indexOf("jdFruitShareCodes") > 0) await download_jdFruit();
+    if (remoteContent.indexOf("jdPetShareCodes") > 0) await download_jdPet();
+    if (remoteContent.indexOf("jdPlantBeanShareCodes") > 0) await download_jdPlant();
+    if (remoteContent.indexOf("jdSuperMarketShareCodes") > 0) await download_jdMarket();
+    if (remoteContent.indexOf("jdFactoryShareCodes") > 0) await download_jdFactory();
+    if (remoteContent.indexOf("new Env('京喜工厂')" > 0)) injectAutoShareCode("jxfactory");
 }
 
 async function download_jdcookie() {
@@ -61,21 +63,21 @@ async function download_jdFruit() {
     let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/jdFruitShareCodes.js");
     let fcontent = response.data;
     await fs.writeFileSync("./jdFruitShareCodes.js", fcontent, "utf8");
-    injectAutoShareCode(content, "farm");
+    injectAutoShareCode("farm");
     console.log("下载农场分享码代码完毕");
 }
 async function download_jdPet() {
     let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/jdPetShareCodes.js");
     let fcontent = response.data;
     await fs.writeFileSync("./jdPetShareCodes.js", fcontent, "utf8");
-    injectAutoShareCode(content, "pet");
+    injectAutoShareCode("pet");
     console.log("下载萌宠分享码代码完毕");
 }
 async function download_jdPlant() {
     let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/jdPlantBeanShareCodes.js");
     let fcontent = response.data;
     await fs.writeFileSync("./jdPlantBeanShareCodes.js", fcontent, "utf8");
-    injectAutoShareCode(content, "bean");
+    injectAutoShareCode("bean");
     console.log("下载种豆得豆分享码代码完毕");
 }
 async function download_jdMarket() {
@@ -88,11 +90,11 @@ async function download_jdFactory() {
     let response = await axios.get("https://github.com/lxk0301/jd_scripts/raw/master/jdFactoryShareCodes.js");
     let fcontent = response.data;
     await fs.writeFileSync("./jdFactoryShareCodes.js", fcontent, "utf8");
-    injectAutoShareCode(content, "ddfactory");
+    injectAutoShareCode("ddfactory");
     console.log("下载京小超分享码代码完毕");
 }
-function injectAutoShareCode(content, type) {
-    if (!content || !type) return;
+function injectAutoShareCode(type) {
+    if (!type) return;
     let pointer = {
         ddfactory: {
             uuid: "item.assistTaskDetailVo.taskToken",
@@ -115,6 +117,7 @@ function injectAutoShareCode(content, type) {
         key: target.match,
         value: `${target.match}\n$.get({url:'http://api.turinglabs.net/api/v1/jd/'+${type}+'/create/'+${match.target}+'/'}, (err, resp, data) => {console.log(data)});`,
     });
+    console.log("互助码随机互助API请求导入完毕");
 }
 
 module.exports = {
