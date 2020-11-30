b7330410613bf07e292565b81e74dd70517263d8
ys <sazs@live.cn>
2020-11-30 11:39:45 +0800

    ~

--- test/qqread.js
+++ test/qqread.js
@@ -63,7 +63,7 @@ console.log(
     })} =========\n`
 );
 
-const logs = 0; //0为关闭日志，1为开启
+const logs = 1; //0为关闭日志，1为开启
 const notifyInterval = 2; //0为关闭通知，1为所有通知，2为宝箱领取成功通知，3为宝箱每15次通知一次
 
 const jbid = 1; //换号则修改这个值,默认账号1
@@ -230,7 +230,7 @@ function qqreadinfo() {
             timeout: 60000,
         };
         $.get(toqqreadinfourl, (error, response, data) => {
-            if (logs) $.log(`${jsname}, 用户名: ${data}`);
+            if (logs) $.log(`${jsname}, 用户名: `, data);
             info = JSON.parse(data);
             tz += "【用户信息】:" + info.data.user.nickName + "\n";
 
