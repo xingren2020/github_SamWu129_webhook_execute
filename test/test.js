f7709bcb04055c5f1a7a31aebf33f7ec5e1f78f2
ys <sazs@live.cn>
2020-12-01 16:36:50 +0800

    ~

--- test/test.js
+++ test/test.js
@@ -1,21 +1,24 @@
-const axios = require("axios");
-const fs = require("fs");
-const stupid = require("../stupid");
+// const axios = require("axios");
+// const fs = require("fs");
+// const stupid = require("../stupid");
 
-async function downloadFile() {
-    var url = [
-        "https://github.com/lxk0301/jd_scripts/raw/master/jd_pet.js",
-        "https://github.com/lxk0301/jd_scripts/raw/master/jd_jdfactory.js",
-        "https://github.com/lxk0301/jd_scripts/raw/master/jd_dreamFactory.js",
-        "https://github.com/lxk0301/jd_scripts/raw/master/jd_plantBean.js",
-        "https://github.com/lxk0301/jd_scripts/raw/master/jd_fruit.js",
-    ];
-    let response = await axios.get(url[0]);
-    let content = response.data;
-    content = content.replace("console.log(`\\n【您的互助码shareCode】 ${$.petInfo.shareCode}\\n`);", "//已替换的代码");
-    // content = await stupid.magic(content);
-    console.log(content);
-    return "end";
-}
+// async function downloadFile() {
+//     var url = [
+//         "https://github.com/lxk0301/jd_scripts/raw/master/jd_pet.js",
+//         "https://github.com/lxk0301/jd_scripts/raw/master/jd_jdfactory.js",
+//         "https://github.com/lxk0301/jd_scripts/raw/master/jd_dreamFactory.js",
+//         "https://github.com/lxk0301/jd_scripts/raw/master/jd_plantBean.js",
+//         "https://github.com/lxk0301/jd_scripts/raw/master/jd_fruit.js",
+//     ];
+//     let response = await axios.get(url[0]);
+//     let content = response.data;
+//     content = content.replace("console.log(`\\n【您的互助码shareCode】 ${$.petInfo.shareCode}\\n`);", "//已替换的代码");
+//     // content = await stupid.magic(content);
+//     console.log(content);
+//     return "end";
+// }
 
-downloadFile();
+// downloadFile();
+console.log(process.env.TEST_KEY);
+console.log("测试结果:", process.env.TEST_KEY == "Cm9wLtO9OlLZI");
+console.log("测试结束");
