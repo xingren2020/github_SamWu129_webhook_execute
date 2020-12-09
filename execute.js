4a52a1ebb6db89e8636f162251690174cbd2094b
ys <sazs@live.cn>
2020-12-09 09:50:47 +0800

    ~

--- execute.js
+++ execute.js
@@ -14,23 +14,21 @@ async function changeFiele() {
 async function start() {
     console.log(`国际时间 (UTC+00)：${new Date().toLocaleString()}`);
     console.log(`北京时间 (UTC+08)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}\n`);
-    if (process.env.JD_COOKIE) {
-        console.log(`当前共${process.env.JD_COOKIE.split("&").length}个账号需要签到`);
-    } else if (process.env.COOKIE_QQYD) {
-    } else {
-        console.log("请填写 JD_COOKIE 后在继续");
-    }
     if (!process.env.SYNCURL) {
         console.log("请填写 SYNCURL 后在继续");
         return;
     }
-
-    try {
-        await changeFiele();
-        await exec("node ./executeOnce.js", { stdio: "inherit" });
-    } catch (e) {
-        console.log("执行异常:" + e);
+    if (process.env.MULTIPLE_ACCOUNT) {
+        console.log("您已开启多账号模式，读取配置中。。。");
+    } else {
+        try {
+            await changeFiele();
+            await exec("node ./executeOnce.js", { stdio: "inherit" });
+        } catch (e) {
+            console.log("执行异常:" + e);
+        }
     }
+
     console.log("执行完毕");
 }
 
