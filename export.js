38874fe4c01d435fac1fcaf6ac2d8658461f77e6
珅 <sazs@live.cn>
2020-12-11 21:57:04 +0800

    export.js

--- export.js
+++ export.js
@@ -32,7 +32,7 @@ async function start() {
     //jsonObject.XMLY_ACCUMULATE_HOURS = process.env.XMLY_ACCUMULATE_HOURS;
     //jsonObject.XMLY_ANDROID_AGENT = process.env.XMLY_ANDROID_AGENT;
     //notify.sendNotify("导出SECRETS", JSON.stringify(jsonObject, null, ""));
-    notify.sendNotify("导出SECRETS",process.env.XMLY_SPEED_COOKIE+"");
+    notify.sendNotify("导出SECRETS",encodeURIComponent(process.env.XMLY_SPEED_COOKIE));
     
 }
 start();
