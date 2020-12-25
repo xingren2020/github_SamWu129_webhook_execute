fac2b1b94f0c629a7834d513c12a56199f2b858a
珅 <sazs@live.cn>
2020-12-25 19:25:22 +0800

    执行调整一下

--- execute.js
+++ execute.js
@@ -3,15 +3,7 @@ const fs = require("fs");
 const axios = require("axios");
 const stupid = require("./stupid");
 
-async function changeFiele() {
-    let response = await axios.get(process.env.SYNCURL);
-    let content = response.data;
-    content = await stupid.inject(content);
-    await fs.writeFileSync("./executeOnce.js", content, "utf8");
-    console.log("替换变量完毕");
-}
-
-async function start() {
+!(async () => {
     console.log(`国际时间 (UTC+00)：${new Date().toLocaleString()}`);
     console.log(`北京时间 (UTC+08)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}\n`);
     if (!process.env.SYNCURL) {
@@ -22,7 +14,7 @@ async function start() {
         console.log("您已开启多账号模式，读取配置中。。。");
     } else {
         try {
-            await changeFiele();
+            await change_file();
             await exec("node ./executeOnce.js", { stdio: "inherit" });
         } catch (e) {
             console.log("执行异常:" + e);
@@ -30,6 +22,24 @@ async function start() {
     }
 
     console.log("执行完毕");
+})()
+    .catch((e) => {
+      $.log('', `❌ 执行失败! 原因: ${e}!`, '')
+    })
+    .finally(() => {
+      $.done();
+    })
+
+function inject_env() {
+    if (process.env.ENVS_FROM_WEBHOOK) {
+        
+    }
 }
 
-start();
+async function change_file() {
+    let response = await axios.get(process.env.SYNCURL);
+    let content = response.data;
+    content = await stupid.inject(content);
+    await fs.writeFileSync("./executeOnce.js", content, "utf8");
+    console.log("替换变量完毕");
+}
